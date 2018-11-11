5599c9561a5a66c04229ae39099cfe571c9894b6
Lewis <Lewis@MacBook-Pro.local>
2018-11-11 14:45:33 +0000

    first draft

---
+++ VirtualBoxInstall.sh
@@ -0,0 +1,21 @@
+function installdmg {
+    set -x
+    tempd=$(mktemp -d)
+    curl $1 > $tempd/pkg.dmg
+    listing=$(sudo hdiutil attach $tempd/pkg.dmg | grep Volumes)
+    volume=$(echo "$listing" | cut -f 3)
+    if [ -e "$volume"/*.app ]; then
+      sudo cp -rf "$volume"/*.app /Applications
+    elif [ -e "$volume"/*.pkg ]; then
+      package=$(ls -1 | grep *.pkg | head -1)
+      sudo installer -pkg "$volume"/"$package".pkg -target /
+    fi
+    sudo hdiutil detach "$(echo "$listing" | cut -f 1)"
+    rm -rf $tempd
+    set +x
+}
+
+echo "Getting download link"
+DOWNLOAD_LINK=$(curl https://www.virtualbox.org/wiki/Downloads | grep 'OSX.dmg' | awk -F'"' '{print $4}') 
+echo "Installing DMG"
+installdmg $DOWNLOAD_LINK
